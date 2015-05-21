05c227d5d31ebc45ed5019a73f4c8cfc7fb7469f
Maria Zamora <Zamora.Mariac@gmail.com>
2015-05-20 21:18:25 -0500

    more edits

---
+++ main.js
@@ -0,0 +1,21 @@
+function getTime(){
+        		var now = new Date();
+                var h = now.getHours();
+                var m = now.getMinutes();
+                var s = now.getSeconds();
+               
+                m = checkTime(m);
+                s = checkTime(s);
+               
+                document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+               
+                setTimeout("getTime()", 1000);
+        }
+       
+        	function checkTime(time){
+                if(time<10){
+                  time = "0" + time;     
+                }
+               
+                return time;
+        }
