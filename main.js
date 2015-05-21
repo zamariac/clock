ef3f1d2600fb7cf65f3f04585c7ec564b1ae0917
Maria Zamora <Zamora.Mariac@gmail.com>
2015-05-21 17:13:30 -0500

    updates

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
-function getTime(){
-        		var now = new Date();
-                var h = now.getHours();
-                var m = now.getMinutes();
-                var s = now.getSeconds();
+function clock(){
+    console.log("clock")
+     var myDate = new Date();
+console.log(myDate);
+
+}
+
+
+
+var intervalId= setInterval (clock, 1000);
+
+
+
+
+// function getTime(){
+//         		var now = new Date();
+//                 var h = now.getHours();
+//                 var m = now.getMinutes();
+//                 var s = now.getSeconds();
                
-                m = checkTime(m);
-                s = checkTime(s);
+//                 m = checkTime(m);
+//                 s = checkTime(s);
                
-                document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+//                 document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
                
-                setTimeout("getTime()", 1000);
-        }
+//                 setTimeout("getTime()", 1000);
+//         }
        
-        	function checkTime(time){
-                if(time<10){
-                  time = "0" + time;     
-                }
+//         	function checkTime(time){
+//                 if(time<10){
+//                   time = "0" + time;     
+//                 }
                
-                return time;
-        }
+//                 return time;
+//         }
